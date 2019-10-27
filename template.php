5fe4c7afb0fa08637dcbaed6776fd3fc91d35547
Alvaro Bolanos <alvarobolanos@gmail.com>
2019-10-27 15:35:32 -0400

    Modified template to latest css changes.

--- template.php
+++ template.php
@@ -66,7 +66,7 @@
 					<a href="activities.html" class="button">More Activities</a>
 				</div>
 			</div>
-			<div>
+			<div class="flex-container">
 				<figure>
 					<div id="highlightDistricts" class="highlight_image"></div>
 					<figcaption>Districts Highlight Image</figcaption>
@@ -80,7 +80,7 @@
 					<a href="districts.html" class="button">Check Out The Districts</a>
 				</div>
 			</div>
-			<div>
+			<div class="flex-container">
 				<figure>
 					<div id="highlightHistory" class="highlight_image"></div>
 					<figcaption>History Highlight Image</figcaption>
@@ -199,20 +199,225 @@
 				</li>
 			</ul>
 		</section>
+
+		<!-- Getting Around -->
+
+		<section id="gettingAround" class="container">
+			<div class="tabs">
+				<!-- Tabs -->
+				<button class="tabLink" onclick="selectTab(event, 'carRental')">
+					<h2>Car Rental</h2>
+				</button>
+				<button class="tabLink" onclick="selectTab(event, 'publicTransportation')">
+					<h2>Public Transportation</h2>
+				</button>
+
+				<!-- Tabs Content -->
+				<!-- Car Rental Tab Content -->
+				<div id="carRental" class="tabContent flex-container">
+					<div>
+						<br />
+						<br />
+						<br />
+						<br />
+						<h3>Car Rental 1</h3>
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo magni eveniet harum
+							officia blanditiis sequi voluptatum aspernatur eaque expedita ut aperiam quo dolorum
+							accusantium cumque alias sapiente, velit minus architecto.</p>
+					</div>
+					<div>
+						<figure>
+							<div id="carRentalImage1" class="tabImages"></div>
+							<figcaption>Car Rental Image 1</figcaption>
+						</figure>
+					</div>
+					<div>
+						<h3>Car Rental 2</h3>
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo magni eveniet harum
+							officia blanditiis sequi voluptatum aspernatur eaque expedita ut aperiam quo dolorum
+							accusantium cumque alias sapiente, velit minus architecto.</p>
+					</div>
+					<div>
+						<figure>
+							<div id="carRentalImage2" class="tabImages"></div>
+							<figcaption>Car Rental Image 2</figcaption>
+						</figure>
+					</div>
+				</div>
+				<!-- Public Transporation Tab Content -->
+				<div id="publicTransportation" class="tabContent flex-container">
+					<div>
+						<br />
+						<br />
+						<br />
+						<br />
+						<h3>Public Transportation 1</h3>
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo magni eveniet
+							harum officia blanditiis sequi voluptatum aspernatur eaque expedita ut aperiam
+							quo dolorum accusantium cumque alias sapiente, velit minus architecto.</p>
+					</div>
+					<div>
+						<figure>
+							<div id="publicTransporationImage1" class="tabImages"></div>
+							<figcaption>Public Transportation Image 1</figcaption>
+						</figure>
+					</div>
+					<div>
+						<h3>Public Transportation 2</h3>
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo magni eveniet
+							harum officia blanditiis sequi voluptatum aspernatur eaque expedita ut aperiam
+							quo dolorum accusantium cumque alias sapiente, velit minus architecto.</p>
+					</div>
+					<div>
+						<figure>
+							<div id="publicTransporationImage2" class="tabImages"></div>
+							<figcaption>Public Transportation Image 2</figcaption>
+						</figure>
+					</div>
+				</div>
+		</section>
 		<!-- Book -->
-		<section id="book">
-			<form action="" method="post" class="container">
-				<fieldset>
-					<legend>
-						<h3>Book</h3>
-					</legend>
-				</fieldset>
-			</form>
+		<section id="book" class="container">
+			<div class="flex-container">
+				<div>
+					<form action="" method="post" class="container">
+						<fieldset>
+							<legend>
+								<h2>Personal Details</h2>
+							</legend>
+							<div class="container">
+								<label for="firstName">
+									<p>First Name:</p>
+								</label>
+								<input id="firstName" type="text" /><br />
+								<label for="middleName">
+									<p>Middle Name:</p>
+								</label>
+								<input id="middleName" type="text" /><br />
+								<label for="lastName">
+									<p>Last Name:</p>
+								</label>
+								<input id="lastName" type="text" /><br />
+								<label for="email">
+									<p>Email:</p>
+								</label>
+								<input id="email" type="email" /><br />
+							</div>
+						</fieldset>
+
+						<fieldset>
+							<legend>
+								<h2>Ticket Details</h2>
+							</legend>
+							<div class="container">
+								<input type="button" class="button" value="Round Trip">
+								<input type="button" class="button" value="One-Way">
+
+								<label for="numTickets">
+									<p>Number of Tickets:</p>
+								</label>
+								<input id="numTickets" type="number" /><br />
+								<label for="departureDate">
+									<p>Departure Date</p>
+								</label>
+								<input id="departureDate" type="date" /><br />
+								<label for="returnDate">
+									<p>Return Date</p>
+								</label>
+								<input id="returnDate" type="date" /><br />
+
+							</div>
+						</fieldset>
+					</form>
+				</div>
+				<div>
+					<table>
+						<thead>
+							<h2>Calcualted Cost</h2>
+						</thead>
+						<tr>
+							<td colspan="2">
+								<h3># (Round Trip/One-way) Ticket(s)</h3>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<h3>Departure Date:</h3>
+							</td>
+							<td>
+								<p>Departure date</p>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<h3>Return Date:</h3>
+							</td>
+							<td>
+								<p>Return date</p>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<h3>Flat Fee per Ticket: </h3>
+							</td>
+							<td>
+								<p>$1.00</p>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<h3>Number of Tickets: </h3>
+							</td>
+							<td>
+								<p>Number of Tickets</p>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<h3>Tax 10%:</h3>
+							</td>
+							<td>
+								<p>Number of Tickets</p>
+							</td>
+						</tr>
+						<tr>
+							<td colspan="2">
+								<hr>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<h3>Total Cost:</h3>
+							</td>
+							<td>
+								<p>Total Cost</p>
+							</td>
+						</tr>
+					</table>
+					<a href="" class="button">Purchase Ticket(s)</a>
+				</div>
+			</div>
+
 		</section>
 
 	</article>
 	<!-- Footer -->
 	<?php include("footer.html"); ?>
+	<script>
+		function selectTab(evt, selectedTab) {
+			var i, tabContent, tabLink;
+			tabContent = document.getElementsByClassName("tabContent");
+			for (i = 0; i < tabContent.length; i++) {
+				tabContent[i].style.display = "none";
+			}
+			tabLink = document.getElementsByClassName("tabLink");
+			for (i = 0; i < tabLink.length; i++) {
+				tabLink[i].className = tabLink[i].className.replace(" active", "");
+			}
+			document.getElementById(selectedTab).style.display = "block";
+			evt.currentTarget.className += " active";
+		}
+	</script>
 </body>
 
 </html>
